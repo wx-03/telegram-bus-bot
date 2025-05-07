ecdb2f8eb8a682d3f804a36ee1f856dbef40a0ad
wen xi <tngwenxi@gmail.com>
2025-05-08 01:20:41 +0800

    Create script to set webhook

---
+++ set_webhook.py
@@ -0,0 +1,11 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+URL = os.getenv("URL")
+
+response = requests.post(f"https://api.telegram.org/bot{BOT_TOKEN}/setWebhook", params={"url": URL})
+print(response.json())
