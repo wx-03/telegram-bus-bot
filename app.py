21da841d875c10b177ccbb9ee17e8c836bca5fa3
wen xi <tngwenxi@gmail.com>
2025-05-08 15:31:31 +0800

    Move try-except to message_handling

--- app.py
+++ app.py
@@ -9,10 +9,7 @@ app = Flask(__name__)
 @app.route("/webhook", methods=["POST"])
 def webhook():
 	data = request.get_json()
-	try:
-		handle_message(data)
-	except Exception as e:
-		send_message(data['message']['chat']['id'], str(e))
+	handle_message(data)
 	return "OK", 200
 
 if __name__ == "__main__":
--- telegram_utils/message_handling.py
+++ telegram_utils/message_handling.py
@@ -1,18 +1,30 @@
 import json
 
-from .commands import handle_command
+from .commands import handle_command, handle_callback_query
 from .messaging import send_message
-
 
 def handle_message(data):
     print(json.dumps(data, indent=4))
-    if not 'text' in data['message']:
-        raise Exception('Sorry, I can only understand text :(')
-    chatid = data['message']['chat']['id']
-    message_text = data['message']['text']
-    
-    if message_text.startswith('/'):
-        message_text_list = message_text.strip().split() # Split command with whitespace as separator
-        command_word = message_text_list[0][1::]
-        args = message_text_list[1::]
-        handle_command(chatid, command_word, args)
+
+    if 'callback_query' in data:
+        chat_id = data['callback_query']['message']['chat']['id']
+        try:
+            handle_callback_query(data['callback_query'])
+        except Exception as e:
+            send_message(chat_id, str(e))
+
+    if 'message' in data:
+        chat_id = data['message']['chat']['id']
+        try:
+            if not 'text' in data['message']:
+                raise Exception('Sorry, I can only understand text :(')
+            message_text = data['message']['text']
+        
+            if message_text.startswith('/'):
+                message_text_list = message_text.strip().split() # Split command with whitespace as separator
+                command_word = message_text_list[0][1::]
+                args = message_text_list[1::]
+                handle_command(chat_id, command_word, args)
+        except Exception as e:
+            send_message(chat_id, str(e))
+
