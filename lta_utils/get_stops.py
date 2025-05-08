fe4c178040114359a99bc95883f14773ef3c2285
wen xi <tngwenxi@gmail.com>
2025-05-08 12:37:47 +0800

    Create bus stops json file

---
+++ lta_utils/get_stops.py
@@ -0,0 +1,25 @@
+import os
+import dotenv
+import requests
+import json
+
+dotenv.load_dotenv()
+API_KEY = os.getenv('API_KEY')
+URL = "https://datamall2.mytransport.sg/ltaodataservice/BusStops"
+headers = {
+  'AccountKey': API_KEY,
+  'Accept': 'application/json'
+}
+
+i = 0
+all_stops = []
+while True:
+    response = requests.request("GET", f'{URL}?$skip={i * 500}', headers=headers)
+    data = response.json()['value']
+    if not data:
+        break
+    all_stops.extend(data)
+    i += 1
+
+with open("./bus_stops.json", "w") as f:
+    json.dump(all_stops, f, indent=4)
